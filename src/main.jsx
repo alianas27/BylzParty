634cafbdadd486cabb35ebccb76987015e046980
Anas Ali <ali.anas67@gmail.com>
2025-06-09 12:08:49 +0530

    Main and Package

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { HashRouter } from 'react-router-dom'; // ✅ Import HashRouter
+import { HashRouter } from 'react-router-dom'; // ⬅️ use HashRouter for GH Pages
 import './index.css';
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter basename="/BylzParty/"> {/* ✅ Add basename */}
+    <HashRouter>
       <App />
     </HashRouter>
   </StrictMode>
